b4599fe658ae28a498e12b3514dae0c31eba37de
MisterSquishy <pdavids@comcast.net>
2018-01-30 22:03:33 -0500

    primitive LOS

--- Assets/Troll Bridge Studios/2D Starter Kit/_Scripts/AI/Movement/Be_Scooter.cs
+++ Assets/Troll Bridge Studios/2D Starter Kit/_Scripts/AI/Movement/Be_Scooter.cs
@@ -23,6 +23,7 @@ namespace TrollBridge
         private Character character;
         private Character_Stats charStats;
         private List<Character> listCharacter = new List<Character>();
+        private bool Seen_Something;
 
         void Start()
         {
@@ -53,43 +54,96 @@ namespace TrollBridge
                 // IF the List of CharacterTypes is greater than 0.
                 if (listCharacter.Count > 0)
                 {
-                    // Create a GameObject variable.
-                    GameObject _character = null;
-                    // Get the closest GameObject with the CharacterType chosen and save it to _character.
-                    _character = Character_Manager.GetClosestCharacterType(character.transform, TypeToFollow, _character, AggroDistance);
-                    // IF the closest gameobject is not null.
-                    if (_character != null)
                     {
-                        float distance = (transform.position - _character.GetComponent<Character>().characterEntity.transform.position).magnitude;
-                        if (distance > ComfortZoneEnd)
+                        Seen_Something = false;
+                        Look_Around();
+                        if (Seen_Something)
                         {
-                            // Move the actual character of this gameobject closer to _character gameobject.
-                            character.characterEntity.transform.position =
-                                Vector2.MoveTowards(transform.position, _character.GetComponent<Character>().characterEntity.transform.position, Time.deltaTime * Speed);
-                        }
-                        else if (distance < ComfortZoneStart)
+                            //do stuff
+                        } else
                         {
-                            // Move the actual character of this gameobject further from _character gameobject.
-                            character.characterEntity.transform.position =
-                                Vector2.MoveTowards(transform.position, _character.GetComponent<Character>().characterEntity.transform.position, Time.deltaTime * Speed * -1);
+                            Wander();
                         }
                     }
                 }
             }
         }
 
-        void Get_Leashed()
+        void Wander()
         {
-            List<Character> heroList = new List<Character>();
-            HingeJoint2D leash = GetComponent<HingeJoint2D>();
-            leash.autoConfigureConnectedAnchor = true;
-
             // Create a GameObject variable.
             GameObject _character = null;
             // Get the closest GameObject with the CharacterType chosen and save it to _character.
             _character = Character_Manager.GetClosestCharacterType(character.transform, TypeToFollow, _character, AggroDistance);
-            leash.connectedBody = _character.GetComponent<Rigidbody2D>();
-            leash.enabled = true;
+            // IF the closest gameobject is not null.
+            if (_character != null)
+            {
+                float distance = (transform.position - _character.GetComponent<Character>().characterEntity.transform.position).magnitude;
+                if (distance > ComfortZoneEnd)
+                {
+                    // Move the actual character of this gameobject closer to _character gameobject.
+                    character.characterEntity.transform.position =
+                        Vector2.MoveTowards(transform.position, _character.GetComponent<Character>().characterEntity.transform.position, Time.deltaTime * Speed);
+                }
+                else if (distance < ComfortZoneStart)
+                {
+                    // Move the actual character of this gameobject further from _character gameobject.
+                    character.characterEntity.transform.position =
+                        Vector2.MoveTowards(transform.position, _character.GetComponent<Character>().characterEntity.transform.position, Time.deltaTime * Speed * -1);
+                }
+            }
+        }
+
+        void Get_Leashed()
+        {
+            //List<Character> heroList = new List<Character>();
+            //HingeJoint2D leash = GetComponent<HingeJoint2D>();
+            //leash.autoConfigureConnectedAnchor = true;
+
+            // Create a GameObject variable.
+            //GameObject _character = null;
+            // Get the closest GameObject with the CharacterType chosen and save it to _character.
+            //_character = Character_Manager.GetClosestCharacterType(character.transform, TypeToFollow, _character, AggroDistance);
+            //leash.connectedBody = _character.GetComponent<Rigidbody2D>();
+            //leash.enabled = true;
+        }
+
+        void Look_Around()
+        {
+            Vector3 start = transform.position;
+            int animation_direction = GetComponent<Animator>().GetInteger("Direction"); //this isn't stupid, right?
+            Vector3 direction;
+            switch (animation_direction) {
+                case 1:
+                    direction = Vector3.up;
+                    break;
+                case 2:
+                    direction = Vector3.left;
+                    break;
+                case 3:
+                    direction = Vector3.down;
+                    break;
+                default:
+                    direction = Vector3.right;
+                    break;
+            }
+            float distance = 5f;
+
+
+            HashSet<RaycastHit2D> objectsInSight = new HashSet<RaycastHit2D>();
+            for (float i = -30; i <= 30; i += 1)
+            {
+                Debug.DrawRay(start, Quaternion.AngleAxis(i, Vector3.forward) * direction * distance, Color.red);
+                objectsInSight.UnionWith(Physics2D.RaycastAll(start, Quaternion.AngleAxis(i, direction) * direction, distance));
+            }
+
+            //now iterate over all results to work out what has happened
+            foreach (RaycastHit2D obj in objectsInSight)
+            {
+                Debug.Log(obj);
+
+                //Seen_Something = true;
+            }
         }
     }
 }
