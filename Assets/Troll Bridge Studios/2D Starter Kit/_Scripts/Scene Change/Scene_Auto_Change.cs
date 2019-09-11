48e08f8ae34e7cc727d87549402e830e832c6b5d
MisterSquishy <pdavids@comcast.net>
2019-09-10 22:02:32 -0400

    main menu ripout

---
+++ Assets/Troll Bridge Studios/2D Starter Kit/_Scripts/Scene Change/Scene_Auto_Change.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Auto_Change : MonoBehaviour
+{
+    public float delay = 3;
+    public string NewLevel = "Field";
+    void Start()
+    {
+        StartCoroutine(LoadLevelAfterDelay(delay));
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(NewLevel);
+    }
+}
--- Assets/Troll Bridge Studios/2D Starter Kit/_Scripts/UI/Start_Game.cs
+++ Assets/Troll Bridge Studios/2D Starter Kit/_Scripts/UI/Start_Game.cs
@@ -1,28 +1,43 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace TrollBridge {
+namespace TrollBridge
+{
 
-	/// <summary>
-	/// This is the script that starts it all!!  This controls the start of the saved persistent data.  You click "New Game" and all of your saved data gets removed so the game can be played like it was the first run through.
-	/// The continue button will not be interactable if there is no saved data as there is nothing to continue!
-	/// </summary>
-	public class Start_Game : MonoBehaviour {
-		
-		[Tooltip("The Start scene for when you click New Game.")]
-		public string NewGameSceneName;
+    /// <summary>
+    /// This is the script that starts it all!!  This controls the start of the saved persistent data.  You click "New Game" and all of your saved data gets removed so the game can be played like it was the first run through.
+    /// The continue button will not be interactable if there is no saved data as there is nothing to continue!
+    /// </summary>
+    public class Start_Game : MonoBehaviour
+    {
+
+        [Tooltip("The Start scene for when you click New Game.")]
+        public string NewGameSceneName;
 
 
-		public void NewGame(){
-			// Remove all saved Player Prefs.
-			PlayerPrefs.DeleteAll();
+        public void NewGame()
+        {
+            // Remove all saved Player Prefs.
+            PlayerPrefs.DeleteAll();
 
-			// Clear the Setup.
-			Grid.setup.ClearSetup();
-			// Clear any State Lists.
-			Grid.stateManager.ClearList();
-			// Load the first scene that starts your game.
-			SceneManager.LoadScene (NewGameSceneName);
-		}
-	}
+            // Clear the Setup.
+            Grid.setup.ClearSetup();
+            // Clear any State Lists.
+            Grid.stateManager.ClearList();
+            // Load the first scene that starts your game.
+            SceneManager.LoadScene(NewGameSceneName);
+        }
+
+        public void DelayedNewGame()
+        {
+            StartCoroutine(waiter());
+        }
+
+        IEnumerator waiter()
+        {
+            yield return new WaitForSeconds(2);
+            NewGame();
+        }
+    }
 }
